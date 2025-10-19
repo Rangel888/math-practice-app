d70b477e395359412b464d03b9a5d1ac3250c0de
Rangel888 <rangelj@southwestern.edu>
2025-10-19 17:26:08 -0500

    Added settings card

--- frontend/src/pages/homepage/HomePage.tsx
+++ frontend/src/pages/homepage/HomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Cards from '../../components/MenuCard';
 import { useNavigate } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
@@ -25,6 +26,17 @@ const HomePage: React.FC = () => {
             icon={
               <div style={{ display: 'flex', gap: '0.5rem' }}>
                 <span style={{ fontSize: '3rem' }}>➕➖✖️➗</span>
+              </div>
+            }
+          />
+        </Grid>
+        <Grid>
+          <Cards 
+            title="Problem Settings"
+            description="Set custom settings for Problem Generator"
+            icon={
+              <div style={{ display: 'flex', gap: '0.5rem' }}>
+                <SettingsIcon sx={{ fontSize: 100 }} />
               </div>
             }
           />
